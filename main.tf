b104523350f30fe9960273b36cb984e546513e46
Alvis <alvis-ang@hotmail.com>
2024-05-29 15:23:56 +0800

    d1b_s3_6

--- main.tf
+++ main.tf
@@ -11,15 +11,42 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "example" {
   bucket = "example"
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
+resource "aws_s3_bucket_policy" "example" {
   bucket = aws_s3_bucket.example.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  policy = data.aws_iam_policy_document.example.json
 }
+
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    "sid" : "1"
+
+    "actions" : [
+      "s3:DeleteBucketPolicy",
+      "s3:PutBucketAcl",
+      "s3:PutBucketPolicy",
+      "s3:PutEncryptionConfiguration",
+      "s3:PutObjectAcl"
+    ]
+
+    "effect" : "Deny"
+
+    "resources" : [
+      "arn:aws:s3:::*",
+    ]
+
+    "Condition": {
+        "StringNotEquals": {
+          "aws:ResourceAccount": [
+            data.aws_caller_identity.current.account_id
+
+  }
+
+}
+
