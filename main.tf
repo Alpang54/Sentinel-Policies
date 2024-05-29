932571f98103cf45dc1fed127631f9a065211d0d
Alvis <alvis-ang@hotmail.com>
2024-05-29 15:43:00 +0800

    5

--- main.tf
+++ main.tf
@@ -17,35 +17,11 @@ resource "aws_s3_bucket" "example" {
   bucket = "example"
 }
 
+data "template_file" "s3_bucket_policy" {
+  template = file("./bucket_iam_policy_block_other_accounts.json")  // Path to your policy template file
+}
+
 resource "aws_s3_bucket_policy" "example" {
   bucket = aws_s3_bucket.example.id
-  policy = data.aws_iam_policy_document.example.json
+  policy = data.template_file.s3_bucket_policy.json
 }
-
-
-data "aws_iam_policy_document" "example" {
-  statement {
-    sid = "1"
-
-    actions = [
-      "s3:DeleteBucketPolicy",
-      "s3:PutBucketAcl",
-      "s3:PutBucketPolicy",
-      "s3:PutEncryptionConfiguration",
-      "s3:PutObjectAcl"
-    ]
-
-    effect = "Deny"
-
-    resources = [
-      "arn:aws:s3:::*",
-    ]
-
-    Condition = {
-        StringNotEquals = {
-          "aws:ResourceAccount" = data.aws_caller_identity.current.account_id
-      }
-    }
-  }
-}
-
