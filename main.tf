ffb6cc76aa846dd411ec7965c2f13d23bae73fcd
Alvis <alvis-ang@hotmail.com>
2024-05-29 15:50:32 +0800

    9

--- main.tf
+++ main.tf
@@ -17,11 +17,38 @@ resource "aws_s3_bucket" "example" {
   bucket = "example"
 }
 
-data "template_file" "s3_bucket_policy" {
-  template = file("./bucket_iam_policy_block_other_accounts.json")  // Path to your policy template file
+resource "aws_iam_policy" "policy" {
+  name        = "sample-policy"
+  description = "My test policy"
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "1",
+      "Effect": "Deny",
+      "Action": [
+        "s3:DeleteBucketPolicy",
+        "s3:PutBucketAcl",
+        "s3:PutBucketPolicy",
+        "s3:PutEncryptionConfiguration",
+        "s3:PutObjectAcl"
+      ],
+      "Resource": [
+        "arn:aws:s3:::*"
+      ],
+      "Condition": {
+        "StringNotEquals": {
+          "aws:ResourceAccount": "${data.aws_caller_identity.current.account_id}"
+        }
+      }
+    }
+  ]
+}
+EOT
 }
 
 resource "aws_s3_bucket_policy" "example" {
   bucket = aws_s3_bucket.example.id
-  policy = data.template_file.s3_bucket_policy.template
+  policy = aws_iam_policy.policy.arn
 }
