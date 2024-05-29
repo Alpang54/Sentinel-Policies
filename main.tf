0a8d1f84f1e1f7d9b24c0fce3d6ddb2b42f2e3ff
Alvis <alvis-ang@hotmail.com>
2024-05-29 10:51:53 +0800

    basic s3

--- main.tf
+++ main.tf
@@ -1,51 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.1"
-    }
-  }
-}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-data "aws_iam_policy_document" "topic" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-
-    actions   = ["SNS:Publish"]
-    resources = ["arn:aws:sns:*:*:s3-event-notification-topic"]
-
-    condition {
-      test     = "ArnLike"
-      variable = "aws:SourceArn"
-      values   = [aws_s3_bucket.bucket.arn]
-    }
-  }
-}
-resource "aws_sns_topic" "topic" {
-  name   = "s3-event-notification-topic"
-  policy = data.aws_iam_policy_document.topic.json
-}
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "your-bucket-name"
-}
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.bucket.id
-
-  topic {
-    topic_arn     = aws_sns_topic.topic.arn
-    events        = ["s3:ObjectCreated:*"]
-    filter_suffix = ".log"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
