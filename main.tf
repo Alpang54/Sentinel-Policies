e494cd41471d1f4a6c2070aed3730bce51329f03
Alvis <alvis-ang@hotmail.com>
2024-05-29 11:32:44 +0800

    d1b_s3_8

--- main.tf
+++ main.tf
@@ -12,10 +12,14 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
+  bucket = "example"
+}
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
