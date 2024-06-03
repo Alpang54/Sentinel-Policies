d76120169fcafd156bddf10a3efa9110eed3bad1
Alvis <alvis-ang@hotmail.com>
2024-06-03 10:49:20 +0800

    2

--- main.tf
+++ main.tf
@@ -25,17 +25,48 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "example" {
+  name               = "eks-cluster-example"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-  name               = "example"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.example.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.example.name
 }
 
 resource "aws_eks_cluster" "example" {
-  name     = "example-cluster"
+  name     = "example"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
+    subnet_ids = ["vpc-0e8b76bbc87a39252", "vpc-0ad8d9aa047f2d4db"]
     endpoint_private_access = true
     endpoint_public_access  = false
-    # ... other configuration ...
   }
+  
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
 }
+
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
+}
+
+
+
