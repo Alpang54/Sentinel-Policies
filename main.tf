786a9a13483060f3e78fbdb045fb14c4a29c3029
Alvis <alvis-ang@hotmail.com>
2024-06-03 12:47:31 +0800

    pass

--- main.tf
+++ main.tf
@@ -11,11 +11,40 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_ecr_repository" "foo" {
-  name                 = "bar"
-  image_tag_mutability = "MUTABLE"
+resource "aws_ecr_repository" "example" {
+  name = "example-repo"
+}
 
-  image_scanning_configuration {
-    scan_on_push = true
+data "aws_iam_policy_document" "example" {
+  statement {
+    sid    = "new policy"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
+    ]
   }
+}
+
+resource "aws_ecr_repository_policy" "example" {
+  repository = aws_ecr_repository.example.name
+  policy     = data.aws_iam_policy_document.example.json
 }
