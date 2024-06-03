1f2812d566b8dfd9b9470183a327fb8e13ef37b8
Alvis <alvis-ang@hotmail.com>
2024-06-03 13:28:37 +0800

    c9_2_resource_permission_sns_fail

--- main.tf
+++ main.tf
@@ -11,48 +11,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_lambda_permission" "with_sns" {
-  statement_id  = "AllowExecutionFromSNS"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.func.function_name
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.default.arn
-}
-
-resource "aws_sns_topic" "default" {
-  name = "call-lambda-maybe"
-}
-
-resource "aws_sns_topic_subscription" "lambda" {
-  topic_arn = aws_sns_topic.default.arn
-  protocol  = "lambda"
-  endpoint  = aws_lambda_function.func.arn
-}
-
-resource "aws_lambda_function" "func" {
-  filename      = "lambdatest.zip"
-  function_name = "lambda_called_from_sns"
-  role          = aws_iam_role.default.arn
-  handler       = "exports.handler"
-  runtime       = "python3.7"
-}
-
-resource "aws_iam_role" "default" {
-  name = "iam_for_lambda_with_sns"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
+resource "aws_sns_topic" "user_updates" {
+  name = "user-updates-topic"
 }
